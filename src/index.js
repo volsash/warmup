aca77f6e3897db9768af5921346944ee01018bb0
volsash <alexandrvolski@hotmail.com>
2018-09-25 22:22:51 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+if (temperature===-20)
+return (-20 *1.8)+32;
+if (temperature===0)
+return (0 *1.8)+32;
+if (temperature===15)
+return  (15 *1.8)+32;
+if (temperature===40)
+return (40*1.8)+32;
 };
